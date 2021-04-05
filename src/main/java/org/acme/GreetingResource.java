374eff433f23634b0f56f3d360a179a72fb6c652
Guido Terceros Fernandez <guido.terceros@gmail.com>
2021-04-05 09:33:08 -0400

    include ORM Panache

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -1,17 +1,25 @@
 package org.acme;
 
+import java.util.List;
 
+import io.quarkus.panache.common.Sort;
+import org.acme.entities.Fruit;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/hello-resteasy")
+@Path("fruits")
+@ApplicationScoped
+@Produces("application/json")
+@Consumes("application/json")
 public class GreetingResource {
 
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "hello jaxrs";
+    public List<Fruit> get() {
+        return Fruit.listAll(Sort.by("name"));
     }
 }
--- src/test/java/org/acme/GreetingResourceTest.java
+++ src/test/java/org/acme/GreetingResourceTest.java
@@ -4,6 +4,8 @@ import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.core.IsNot.not;
 import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
@@ -11,11 +13,31 @@ public class GreetingResourceTest {
 
     @Test
     public void testHelloEndpoint() {
+        //List all, should have all 3 fruits the database has initially:
         given()
-          .when().get("/hello-resteasy")
-          .then()
-             .statusCode(200)
-             .body(is("Hello RESTEasy"));
+                .when().get("/fruits")
+                .then()
+                .statusCode(200)
+                .body(
+                        containsString("Cherry"),
+                        containsString("Apple"),
+                        containsString("Banana"));
+
+        //Delete the Cherry:
+        given()
+                .when().delete("/fruits/1")
+                .then()
+                .statusCode(204);
+
+        //List all, cherry should be missing now:
+        given()
+                .when().get("/fruits")
+                .then()
+                .statusCode(200)
+                .body(
+                        not(containsString("Cherry")),
+                        containsString("Apple"),
+                        containsString("Banana"));
     }
 
 }
